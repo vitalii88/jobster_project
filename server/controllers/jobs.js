a78a9eb34a487165a1dbf9a9bfb6891fc15566d8
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-10-06 16:12:08 -0400

    feat(server): add pagination

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import JobShcema from '../models/Job.js';
 
 const getSortType = (value) => {
   switch (value) {
@@ -23,8 +24,7 @@ const getSortType = (value) => {
 }
 
 export const getAllJobs = async (req, resp) => {
-  console.log('req.query: ', req.query);
-  const { status, jobType, sort, page, search } = req.query;
+  const { status, jobType, sort, search } = req.query;
 
   const queryObject = {
     createdBy: req.user.userId,
@@ -48,8 +48,17 @@ export const getAllJobs = async (req, resp) => {
   const sortType = getSortType(sort);
   result.sort(sortType);
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const totalJobs = await JobShcema.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
   const jobs = await result;
-  resp.status(StatusCodes.OK).json({ jobs });
+  resp.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 export const getJod = async (req, resp) => {
